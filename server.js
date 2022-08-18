df85583bde0fc5a04625956b2dce7d23a01bfd4c
Gabini - Mac <haile.mak11@gmail.com>
2022-08-18 11:37:20 +0300

    change env var USER to FAVOURITE_USER

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-const user = process.env.USER;
+const user = process.env.FAVOURITE_USER;
 async function main() {
   while(true) {
     console.log(`${user} rocks!`);
