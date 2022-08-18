64f2f769f9a25ebb1a725ec03b632de6e22dfe98
Gabini - Mac <haile.mak11@gmail.com>
2022-08-18 11:32:26 +0300

    update env variable name

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-const user = process.env.user;
+const user = process.env.USER;
 async function main() {
   while(true) {
     console.log(`${user} rocks!`);
